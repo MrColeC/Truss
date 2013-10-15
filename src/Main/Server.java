fead4113d3a49c313d90aaab8f00c5af980f23a0
Cole <colec.mobile@gmail.com>
2013-10-14 21:11:39 -0700

    Fixed UI bug

--- src/Main/Server.java
+++ src/Main/Server.java
@@ -1,5 +1,8 @@
 package Main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 /**
@@ -34,16 +37,17 @@ public class Server extends Thread {
 
 		// Display the UI boilerplate
 		System.out.println("======================================================================");
-		System.out.println("Welcome. This server is accepint connections on port [" + PortUsed + "]");
+		System.out.println("Welcome. This server is accepting connections on port [" + PortUsed + "]");
 		System.out.println("Commands are:");
 		System.out.println("QUIT - Closes connection with the server and quits");
 		System.out.println("* - Anything else is just echo'ed back");
 		System.out.println("======================================================================");
 
 		// Enter the UI loop
+		UserInput = readUI();
 		while ((UserInput != null) && (UserInput.compareToIgnoreCase("quit") != 0)) {
 			System.out.println("You entered:" + UserInput);
-			UserInput = "";
+			UserInput = readUI();
 		}
 		System.exit(0);
 	}
@@ -64,5 +68,40 @@ public class Server extends Thread {
 			UIDcounter++;
 			new ServerThread(subject, mylog, socket, UIDcounter, JobLock).start();
 		}
+	}
+	
+	/**
+	 * Reads input provided by the user, returns a string
+	 * 
+	 * @return
+	 */
+	private String readUI() {
+		System.out.flush();
+		System.out.print("> ");
+		System.out.flush();
+		String data = null;
+		BufferedReader inputHandle = new BufferedReader(new InputStreamReader(System.in));
+		boolean wait = true;
+		while (wait) {
+			try {
+				if (inputHandle.ready()) {
+					wait = false;
+				} else {
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+						mylog.out("ERROR", "Failed to sleep");
+					}
+				}
+			} catch (IOException err) {
+				mylog.out("ERROR", "Failed to check if buffered input was ready [" + err + "]");
+			}
+		}
+		try {
+			data = inputHandle.readLine();
+		} catch (IOException err) {
+			mylog.out("ERROR", "Failed to collect user input [" + err + "]");
+		}
+		return data;
 	}
 }
