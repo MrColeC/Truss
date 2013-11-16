dd6f1ac21c5d0de0d5e68378bb6c91c318748f7b
Cole <colec.mobile@gmail.com>
2013-11-15 20:20:26 -0800

    Better formatting

--- src/Main/JobManagement.java
+++ src/Main/JobManagement.java
@@ -67,11 +67,11 @@ public class JobManagement {
 
 			// Display the data
 			if (mode.equalsIgnoreCase("unassigned")) {
-				System.out.printf( "%8s %2s %50s %n", OS, SecLev, ActualJob);
+				System.out.printf( "%8s|%2s|%-50s%n", OS, SecLev, ActualJob);
 			} else if (mode.equalsIgnoreCase("assigned")) {
-				System.out.printf( "%25s %15s %8s %2s %50s %n", Issued, AssignedOn, OS, SecLev, ActualJob);
+				System.out.printf( "%25s|%15s|%8s|%2s|%-50s %n", Issued, AssignedOn, OS, SecLev, ActualJob);
 			} else if (mode.equalsIgnoreCase("complete")) {
-				System.out.printf( "%5s %50s %n", JobID, ActualJob);
+				System.out.printf( "%5s|%-50s %n", JobID, ActualJob);
 			}
 
 			scan++;
